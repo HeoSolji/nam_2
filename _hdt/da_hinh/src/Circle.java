5833ffd0ef4f9130c079584214dc33fe4185f224
Khiem472 <48560151+Khiem472@users.noreply.github.com>
2019-10-08 00:02:58 +0700

    2

--- _hdt/da_hinh/src/Circle.java
+++ _hdt/da_hinh/src/Circle.java
@@ -1,32 +1,33 @@
+
 public class Circle extends Shape {
-  protected double radius;
-  private static final double PI = 3.14;
+   protected double radius;
+   private static final double PI = 3.14;
 
-  public Circle() {}
-  public Circle(double radius) {
-    setRadius(radius);
-  }
-  public Circle(double radius, String color, boolean filled) {
-    super(color, filled);
-    setRadius(radius);
-  }
+   public Circle() {}
+   public Circle(double radius) {
+      setRadius(radius);
+   }
+   public Circle(double radius, String color, boolean filled) {
+      super(color, filled);
+      setRadius(radius);
+   }
 
-  public void setRadius(double radius) {
-    this.radius = radius;
-  }
-  public double getRadius() {
-    return radius;
-  }
+   public void setRadius(double radius) {
+      this.radius = radius;
+   }
+   public double getRadius() {
+      return radius;
+   }
 
-  public double getArea() {
-    return PI*radius*radius;
-  }
+   public double getArea() {
+      return PI*radius*radius;
+   }
 
-  public double getPerimeter() {
-    return 2*PI*radius;
-  }
-  
-  public String toString() {
-    return "Circle[radius=" + getRadius() + ",color=" + getColor() + ",filled=" + isFilled() + "]";
-  }
+   public double getPerimeter() {
+      return 2*PI*radius;
+   }
+
+   public String toString() {
+      return "Circle[radius=" + getRadius() + ",color=" + getColor() + ",filled=" + isFilled() + "]";
+   }
 }
--- _hdt/da_hinh/src/Rectangle.java
+++ _hdt/da_hinh/src/Rectangle.java
@@ -1,39 +1,39 @@
 public class Rectangle extends Shape {
-  protected double width;
-  protected double length;
+   protected double width;
+   protected double length;
 
-  public Rectangle() {}
-  public Rectangle(double width, double length) {
-    setWidth(width);
-    setLength(length);
-  }
-  public Rectangle(double width, double length, String color, boolean filled) {
-    super(color, filled);
-    setWidth(width);
-    setLength(length);
-  }
+   public Rectangle() {}
+   public Rectangle(double width, double length) {
+      setWidth(width);
+      setLength(length);
+   }
+   public Rectangle(double width, double length, String color, boolean filled) {
+      super(color, filled);
+      setWidth(width);
+      setLength(length);
+   }
 
-  public double getWidth() {
-    return width;
-  }
-  public void setWidth(double width) {
-    this.width = width;
-  }
-  public double getLength() {
-    return length;
-  }
-  public void setLength(double length) {
-    this.length = length;
-  }
+   public double getWidth() {
+      return width;
+   }
+   public void setWidth(double width) {
+      this.width = width;
+   }
+   public double getLength() {
+      return length;
+   }
+   public void setLength(double length) {
+      this.length = length;
+   }
 
-  public double getArea() {
-    return width*length;
-  }
-  public double getPerimeter() {
-    return 2*(width+length);
-  }
+   public double getArea() {
+      return width*length;
+   }
+   public double getPerimeter() {
+      return 2*(width+length);
+   }
 
-  // public String toString() {
-  //   return 
-  // }
+   // public String toString() {
+   //   return
+   // }
 }
