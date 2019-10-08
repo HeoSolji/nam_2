02dd70642006f25a8339fb2938fd5094313d29de
Khiem472 <Khiem472@users.noreply.github.com>
2019-10-08 19:08:28 +0700

    hoàn thành

--- _hdt/da_hinh/src/Main.java
+++ _hdt/da_hinh/src/Main.java
@@ -1,7 +1,7 @@
 class Main {
    public static void main(String[] args) {
       Square s = new Square();
-      s.setSide(2f);
+      s.setSide(4.7);
       s.setColor("red");
       s.setFilled(true);
       System.out.println(s.toString());
