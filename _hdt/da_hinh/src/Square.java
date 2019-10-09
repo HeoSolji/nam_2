7446ab1623f6177323a56ac051a4d108a63a62fb
Nguyễn Hoàng Bá Khiêm <48560151+Khiem472@users.noreply.github.com>
2019-10-09 10:36:32 +0700

    100

--- _hdt/da_hinh/src/Square.java
+++ _hdt/da_hinh/src/Square.java
@@ -4,6 +4,10 @@
 public class Square extends Rectangle {
 
    public Square() {}
+   public Square(double side) {
+       setWidth(side);
+       setLength(side);
+   }
    public Square(double side, String color, boolean filled) {
       setWidth(side);
       setLength(side);
@@ -16,16 +20,19 @@ public class Square extends Rectangle {
    }
    public void setSide(double side) {
       setLength(side);
+      setWidth(side);
    }
 
    @Override
    public void setWidth(double side) {
       super.setWidth(side);
+      super.setLength(side);
    }
 
    @Override
    public void setLength(double side) {
       super.setLength(side);
+      super.setWidth(side);
    }
 
    public String toString() {
